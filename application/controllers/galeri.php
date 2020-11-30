13d755e07f6579ade5c5bdb03f4ded637b820921
nurulhotimah <nurulhotimah629@gmail.com>
2020-11-30 10:36:42 +0700

    menambahkan variabel baru di controller/galeri.php

--- application/controllers/galeri.php
+++ application/controllers/galeri.php
@@ -4,6 +4,10 @@ class Galeri extends CI_Controller
     public  function index()
     {
         $data['title'] = 'Galei';
+        // mengambil data session
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        // untuk menampilkan data dari database
+        $data['berita'] = $this->m_berita->tampil_data()->result();
         $this->load->view('templates/header');
         $this->load->view('templates/sidebar');
         $this->load->view('templates/topbar');
