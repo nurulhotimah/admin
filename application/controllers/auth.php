b5efa73f40ac993168f6a18dd3405bd7c25a7a80
nurulhotimah <nurulhotimah629@gmail.com>
2020-12-02 17:50:05 +0700

    menambahkan pengecekan login

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -12,6 +12,9 @@ class auth extends CI_Controller
 
     public function index()
     {
+        if ($this->session->userdata('email')) {
+            redirect('user');
+        }
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
         $this->form_validation->set_rules('password', 'Password', 'required|trim');
         if ($this->form_validation->run() == false) {
@@ -65,6 +68,9 @@ class auth extends CI_Controller
     }
     public function registration()
     {
+        if ($this->session->userdata('email')) {
+            redirect('user');
+        }
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
             'is_unique' => 'this email has allready regiseted!'
