a9d325b93b472ca189921854e3eb59a542274c54
nurulhotimah <nurulhotimah629@gmail.com>
2020-12-02 21:35:35 +0700

    menambahkan onclick

--- application/views/kelola_data/berita.php
+++ application/views/kelola_data/berita.php
@@ -26,53 +26,18 @@
                         </div>
                         <div class="card-footer">
                             <small class="text-muted"><?= ($B['tanggal']); ?></small>
-                            <a href="">
-                                <?php echo anchor('berita/edit/' . $B['id'], '<div class="btn btn-primary btn-sm ml-5"><i class="fa fa-edit"></i></div>') ?>
+                            <a href="<?php echo base_url(); ?>berita/edit/<?= $B['id']; ?>" class="btn btn-primary btn-sm ml-5"><i class="fa fa-edit"></i>
+
                             </a>
-                            <a onclick="javascript:return confirm('anda yakin ingin menghapusnya?')">
-                                <?php echo anchor('berita/hapus/' . $B['id'], '<div class="btn btn-danger btn-sm ml-3"><i class="fa fa-trash"></i></div>') ?>
+                            <a href="<?php echo base_url(); ?>berita/hapus/<?= $B['id']; ?>" class="btn btn-danger btn-sm ml-5" onclick="return confirm('Anda yakin ingin menghapusnya?');"><i class="fa fa-trash"></i>
+
                             </a>
                         </div>
 
                     </div>
                 </div>
 
-                <div class="col-md-4">
-                    <div class="card">
-                        <img src="<?php echo base_url(); ?>assets/foto/<?php echo ($B['gambar']); ?>" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title"><?= ($B['judul']); ?></h5>
-                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted"><?= ($B['tanggal']); ?></small>
-                            <a href="">
-                                <?php echo anchor('berita/edit/' . $B['id'], '<div class="btn btn-primary btn-sm ml-5"><i class="fa fa-edit"></i></div>') ?>
-                            </a>
-                            <a onclick="javascript:return confirm('anda yakin ingin menghapusnya?')">
-                                <?php echo anchor('berita/hapus/' . $B['id'], '<div class="btn btn-danger btn-sm ml-3"><i class="fa fa-trash"></i></div>') ?>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card">
-                        <img src="<?php echo base_url(); ?>assets/foto/<?php echo ($B['gambar']); ?>" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title"><?= ($B['judul']); ?></h5>
-                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted"><?= ($B['tanggal']); ?></small>
-                            <a href="">
-                                <?php echo anchor('berita/edit/' . $B['id'], '<div class="btn btn-primary btn-sm ml-5"><i class="fa fa-edit"></i></div>') ?>
-                            </a>
-                            <a onclick="javascript:return confirm('anda yakin ingin menghapusnya?')">
-                                <?php echo anchor('berita/hapus/' . $B['id'], '<div class="btn btn-danger btn-sm ml-3"><i class="fa fa-trash"></i></div>') ?>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+
             </div>
 
 
